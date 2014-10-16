5f89c7b54d2979ade9be40375bd00a32a3e65994
uLipe <felipe.projetos@terra.com.br>
2014-10-16 18:07:17 -0300

    Added a memory manager
    Added a port of memory manager, need tests.

--- OsPort.h
+++ OsPort.h
@@ -36,14 +36,14 @@ struct ctx_
 {
 
 	//And these are the sofware context saved registers
-	uint32_t r11;
-	uint32_t r10;
-	uint32_t r9;
-	uint32_t r8;
-	uint32_t r7;
-	uint32_t r6;
-	uint32_t r5;
 	uint32_t r4;
+	uint32_t r5;
+	uint32_t r6;
+	uint32_t r7;
+	uint32_t r8;
+	uint32_t r9;
+	uint32_t r10;
+	uint32_t r11;
 
 	//These are the hardware context saved registers.
 	uint32_t r0;
