2fb762ae43bc8123b68b13df716f7d465c7deb2c
uLipe <ryukokki.felipe@gmail.com>
2017-04-26 12:46:12 -0300

    gpio: added kl25z gpio driver

--- drivers/kinetis_kl25z/kl25z_gpio.c
+++ drivers/kinetis_kl25z/kl25z_gpio.c
@@ -25,58 +25,164 @@
  */
 typedef struct {
 	GPIO_Type *gpio;
+	PORT_Type *port;
 	clock_ip_name_t portClk;
 }KL25ZGpioDevData_t;
 
 static OsStatus_t KL25Z_GpioInit(void *arg)
 {
 	OsStatus_t ret = kStatusOk;
+	Device_t *dev = (Device_t *)arg;
+	KL25ZGpioDevData_t *dat = (KL25ZGpioDevData_t *)dev->config->devConfigData;
+
+	/* gates the selectedd gpio clock */
+	CLOCK_EnableClock(dat->portClk);
+
 	return(ret);
 }
 
 
-static OsStatus_t KL25Z_GpioConfigureRow(Device_t *this, uint8_t bitOffset ,uint8_t len, uint32_t acceptMask,uint32_t configMask )
+static OsStatus_t KL25Z_GpioConfigureRow(Device_t *dev, uint8_t bitOffset ,uint8_t len, uint32_t acceptMask,uint32_t configMask )
 {
 	OsStatus_t ret = kStatusOk;
+	KL25ZGpioDevData_t *dat = (KL25ZGpioDevData_t *)dev->config->devConfigData;
+	gpio_pin_config_t cfg;
+
+	for(uint8_t i = 0; i < len; i++, acceptMask >>= 1) {
+		/* only modify desired pins passed with accept mask */
+		if(acceptMask & 0x01) {
+			/* select configuration */
+			if(configMask & GPIO_DIR_OUT) {
+				cfg.outputLogic = 0;
+				cfg.pinDirection = kGPIO_DigitalOutput;
+				/* select output type */
+				if(configMask & GPIO_OPEN_DRAIN) {
+					ret = kNotImplementedForThisDevice;
+				}
+
+			} else {
+				cfg.outputLogic = 0;
+				cfg.pinDirection = kGPIO_DigitalInput;
+				if(configMask & GPIO_OPEN_DRAIN) {
+					ret = kNotImplementedForThisDevice;
+				} else if (configMask & GPIO_INT_ENABLED) {
+					ret = kNotImplementedForThisDevice;
+				}
+			}
+
+			if(ret != kNotImplementedForThisDevice) {
+				GPIO_PinInit(dat->gpio, i + bitOffset, &cfg);
+			}
+		}
+	}
 	return(ret);
 }
 
 
-static OsStatus_t KL25Z_GpioConfigureSingle(Device_t *this, uint8_t bitPos, uint32_t configMask )
+static OsStatus_t KL25Z_GpioConfigureSingle(Device_t *dev, uint8_t bitPos, uint32_t configMask )
 {
 	OsStatus_t ret = kStatusOk;
+	KL25ZGpioDevData_t *dat = (KL25ZGpioDevData_t *)dev->config->devConfigData;
+	gpio_pin_config_t cfg;
+
+	/* select configuration */
+	if(configMask & GPIO_DIR_OUT) {
+		cfg.outputLogic = 0;
+		cfg.pinDirection = kGPIO_DigitalOutput;
+		/* select output type */
+		if(configMask & GPIO_OPEN_DRAIN) {
+			ret = kNotImplementedForThisDevice;
+		}
+
+	} else {
+		cfg.outputLogic = 0;
+		cfg.pinDirection = kGPIO_DigitalInput;
+		if(configMask & GPIO_OPEN_DRAIN) {
+			ret = kNotImplementedForThisDevice;
+		} else if (configMask & GPIO_INT_ENABLED) {
+			ret = kNotImplementedForThisDevice;
+		}
+	}
+
+	if(ret != kNotImplementedForThisDevice) {
+		GPIO_PinInit(dat->gpio, bitPos, &cfg);
+	}
+
+
 	return(ret);
 }
 
 
-static OsStatus_t KL25Z_GpioRegisterIntCallback(Device_t * this, uint8_t bit, uLipeGpioCallBack_t cb)
+static OsStatus_t KL25Z_GpioRegisterIntCallback(Device_t * dev, uint8_t bit, uLipeGpioCallBack_t cb)
+{
+	OsStatus_t ret = kNotImplementedForThisDevice;
+	(void)dev;
+	(void)bit;
+	(void)cb;
+
+	 /* TODO: Need to understand how Kinetis int pins works */
+	return(ret);
+}
+
+static OsStatus_t KL25Z_GpioWriteRow(Device_t *dev,uint8_t bitOffset ,uint8_t len, uint32_t acceptMask, uint32_t value)
 {
 	OsStatus_t ret = kStatusOk;
+	KL25ZGpioDevData_t *dat = (KL25ZGpioDevData_t *)dev->config->devConfigData;
+
+	/* not necessary here */
+	(void)len;
+	(void)bitOffset;
+
+	/* the kinetis sdk has no support for a write gpio data out register! bizarre, we need to deal
+	 * directly with the register
+	 */
+	uint32_t tmp = dat->gpio->PDOR & (~acceptMask);
+	dat->gpio->PDOR = (tmp | value );
+
+
 	return(ret);
+
 }
 
-static OsStatus_t KL25Z_GpioWriteRow(Device_t *this,uint8_t bitOffset ,uint8_t len, uint32_t acceptMask, uint32_t value)
+static OsStatus_t KL25Z_GpioWriteSingle(Device_t *dev, uint8_t bitPos, uint8_t value )
 {
 	OsStatus_t ret = kStatusOk;
+	KL25ZGpioDevData_t *dat = (KL25ZGpioDevData_t *)dev->config->devConfigData;
+
+	uint32_t tmp = dat->gpio->PDOR & (~(1 << bitPos));
+	tmp |= ((value & 0x01) << bitPos);
+	dat->gpio->PDOR = tmp;
+
 	return(ret);
 }
 
-static OsStatus_t KL25Z_GpioWriteSingle(Device_t *this, uint8_t bitPos, uint8_t value )
-{
-	OsStatus_t ret = kStatusOk;
-	return(ret);
-}
-
-static uint32_t  KL25Z_GpioReadRow(Device_t *this,uint8_t bitOffset ,uint8_t len, uint32_t acceptMask, OsStatus_t *err)
+static uint32_t  KL25Z_GpioReadRow(Device_t *dev,uint8_t bitOffset ,uint8_t len, uint32_t acceptMask, OsStatus_t *err)
 {
 	uint32_t ret = 0;
-	return(ret);
+	KL25ZGpioDevData_t *dat = (KL25ZGpioDevData_t *)dev->config->devConfigData;
+	/* not necessary here */
+	(void)len;
+	(void)bitOffset;
 
+	ret = dat->gpio->PDIR & acceptMask;
+
+	if(err != NULL) {
+		*err = kStatusOk;
+	}
+
+	return(ret);
 }
 
-static bool KL25Z_GpioReadSingle (Device_t *this, uint8_t bitPos, OsStatus_t *err )
+static bool KL25Z_GpioReadSingle (Device_t *dev, uint8_t bitPos, OsStatus_t *err )
 {
 	bool ret = false;
+	KL25ZGpioDevData_t *dat = (KL25ZGpioDevData_t *)dev->config->devConfigData;
+	ret = GPIO_ReadPinInput(dat->gpio, bitPos);
+
+	if(err != NULL) {
+		*err = kStatusOk;
+	}
+
 	return(ret);
 }
 
@@ -97,7 +203,7 @@ GpioDeviceApi_t kl25zGpioDeviceApi = {
 
 static KL25ZGpioDevData_t kl25zGpioData_a = {
 		.gpio = GPIOA,
-		.portClk = kCLOCK_PortA
+		.portClk = kCLOCK_PortA,
 };
 
 static DeviceConfig_t kl25zGpioCfg_a = {
@@ -106,7 +212,7 @@ static DeviceConfig_t kl25zGpioCfg_a = {
 		.earlyInitFcn=KL25Z_GpioInit,
 		.irqOffset=0,
 		.refCount=0,
-		.devConfigData=&kl25zGpioCfg_a,
+		.devConfigData=&kl25zGpioData_a,
 };
 
 ULIPE_DEVICE_DECLARE(gpioGpio0, &kl25zGpioCfg_a, NULL, &kl25zGpioDeviceApi);
@@ -118,7 +224,8 @@ ULIPE_DEVICE_DECLARE(gpioGpio0, &kl25zGpioCfg_a, NULL, &kl25zGpioDeviceApi);
 #if OS_USE_GPIOB_GPIO > 0
 static KL25ZGpioDevData_t kl25zGpioData_b = {
 		.gpio = GPIOB,
-		.portClk = kCLOCK_PortB
+		.portClk = kCLOCK_PortB,
+
 };
 
 static DeviceConfig_t kl25zGpioCfg_b = {
@@ -127,7 +234,7 @@ static DeviceConfig_t kl25zGpioCfg_b = {
 		.earlyInitFcn=KL25Z_GpioInit,
 		.irqOffset=0,
 		.refCount=0,
-		.devConfigData=&kl25zGpioCfg_b,
+		.devConfigData=&kl25zGpioData_b,
 };
 
 ULIPE_DEVICE_DECLARE(gpioGpio1, &kl25zGpioCfg_b, NULL, &kl25zGpioDeviceApi);
@@ -138,6 +245,7 @@ ULIPE_DEVICE_DECLARE(gpioGpio1, &kl25zGpioCfg_b, NULL, &kl25zGpioDeviceApi);
 static KL25ZGpioDevData_t kl25zGpioData_c = {
 		.gpio = GPIOC,
 		.portClk = kCLOCK_PortC
+
 };
 
 static DeviceConfig_t kl25zGpioCfg_c = {
@@ -146,7 +254,7 @@ static DeviceConfig_t kl25zGpioCfg_c = {
 		.earlyInitFcn=KL25Z_GpioInit,
 		.irqOffset=0,
 		.refCount=0,
-		.devConfigData=&kl25zGpioCfg_c,
+		.devConfigData=&kl25zGpioData_c,
 };
 
 ULIPE_DEVICE_DECLARE(gpioGpio2, &kl25zGpioCfg_c, NULL, &kl25zGpioDeviceApi);
@@ -166,7 +274,7 @@ static DeviceConfig_t kl25zGpioCfg_d = {
 		.earlyInitFcn=KL25Z_GpioInit,
 		.irqOffset=0,
 		.refCount=0,
-		.devConfigData=&kl25zGpioCfg_d,
+		.devConfigData=&kl25zGpioData_d,
 };
 
 ULIPE_DEVICE_DECLARE(gpioGpio3, &kl25zGpioCfg_d, NULL, &kl25zGpioDeviceApi);
@@ -178,6 +286,7 @@ ULIPE_DEVICE_DECLARE(gpioGpio3, &kl25zGpioCfg_d, NULL, &kl25zGpioDeviceApi);
 static KL25ZGpioDevData_t kl25zGpioData_e = {
 		.gpio = GPIOE,
 		.portClk = kCLOCK_PortE
+
 };
 
 
@@ -187,7 +296,7 @@ static DeviceConfig_t kl25zGpioCfg_e = {
 		.earlyInitFcn=KL25Z_GpioInit,
 		.irqOffset=0,
 		.refCount=0,
-		.devConfigData=&kl25zGpioCfg_e,
+		.devConfigData=&kl25zGpioData_e,
 };
 
 ULIPE_DEVICE_DECLARE(gpioGpio4, &kl25zGpioCfg_e, NULL, &kl25zGpioDeviceApi);
